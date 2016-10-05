f3f539b962ac9ec659601e938c1345ecef467e0e
Phan Tuan Tu <icoding.active@gmail.com>
2016-10-05 09:30:49 +0700

    cm

--- app/src/main/java/id/grw/com/idgame/MainActivity.java
+++ app/src/main/java/id/grw/com/idgame/MainActivity.java
@@ -2,6 +2,8 @@ package id.grw.com.idgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Hide Action Bar
+        getSupportActionBar().hide();
+        //set image of game
+        ImageView gameImg = (ImageView)findViewById(R.id.gameImg);
+        gameImg.setImageResource(R.drawable.multichoice);
     }
 }
